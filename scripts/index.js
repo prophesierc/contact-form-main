5e56989f72637c0836a9a4fcde693a9c092ecb20
ProphesierC <bwknowles98@gmail.com>
2024-06-04 00:24:13 -0400

    field => input validation (WIP)

--- scripts/index.js
+++ scripts/index.js
@@ -2,17 +2,51 @@ class Page
 {
     constructor() 
     {
-        this.fName = document.querySelectorAll("input[type=text]");
-    }
-    
-    validator() 
-    {
-        this.fName.forEach(element => 
-        {
+
+        this.requiredErrorMessage = document.querySelectorAll("#required-message");
+        this.requiredErrorBorder = document.querySelectorAll("input[type=text], .radio-wrapper");
+        this.submit = document.querySelector("#submit-button");
+        this.errorMapping = Array.from(this.requiredErrorMessage).map((errorMessageElement, index) => {
+            return {
+                errorMessageElement: errorMessageElement,
+                borderElement: this.requiredErrorBorder[index]
+            };
         });
+    }
+    displayError() 
+    {  
+        this.requiredErrorMessage.forEach(textField => 
+        {
+            this.requiredErrorBorder.forEach(borderField => 
+            {
+                borderField.style.border = '1px solid red';
+            });
+            textField.style.display = 'flex';    
+        });
+
+    }
+
+    validator()
+    {
+        console.log(this.errorMapping[0]);   
+        this.errorMapping.forEach(Element => 
+        {
+            Element.style.color = 'green';   
+        })
+        
+    }
+
+    request()
+    {
+        this.submit.addEventListener('click', () => 
+        {
+            // this.displayError();
+            this.validator();
+        })
     }
 
 }
 
-const test = new Page();
-test.validator();
+const page = new Page();
+
+page.request();
