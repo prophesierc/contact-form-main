b505866bef5febe203f0803933e98fa144846e43
ProphesierC <bwknowles98@gmail.com>
2024-06-05 01:03:05 -0400

    error validation functionality mostly completed

--- scripts/index.js
+++ scripts/index.js
@@ -1,52 +1,57 @@
-class Page 
+class User 
 {
     constructor() 
     {
-
-        this.requiredErrorMessage = document.querySelectorAll("#required-message");
-        this.requiredErrorBorder = document.querySelectorAll("input[type=text], .radio-wrapper");
         this.submit = document.querySelector("#submit-button");
-        this.errorMapping = Array.from(this.requiredErrorMessage).map((errorMessageElement, index) => {
-            return {
-                errorMessageElement: errorMessageElement,
-                borderElement: this.requiredErrorBorder[index]
-            };
-        });
+        //fname
+        this.firstNameInput = document.forms['main']['fname-text'];
+        this.firstNameLabel = document.querySelector('#fname-label');
+        //lname
+        this.lastNameInput = document.forms['main']['lname-text'];
+        this.lastNameLabel = document.querySelector('#lname-label');
+        //email
+        this.emailInput = document.forms['main']['email-text'];
+        this.emailLabel = document.querySelector('#email-label');
+        //query
+        this.queryInput = document.forms['main']['query-text'];
+        // this.queryLabel = document.querySelector('#query-label');
+        // wip (index of both queries to validate if some() === true)
+        //message
+        this.messageInput = document.forms['main']['message-text'];
+        this.messageLabel = document.querySelector('#message-label');
+        ///consent
+        this.consentInput = document.forms['main']['consent-text'];
+        this.consentLabel = document.querySelector('#consent-label');
     }
-    displayError() 
-    {  
-        this.requiredErrorMessage.forEach(textField => 
-        {
-            this.requiredErrorBorder.forEach(borderField => 
-            {
-                borderField.style.border = '1px solid red';
-            });
-            textField.style.display = 'flex';    
-        });
 
-    }
-
-    validator()
+    displayError(input, label) 
     {
-        console.log(this.errorMapping[0]);   
-        this.errorMapping.forEach(Element => 
-        {
-            Element.style.color = 'green';   
-        })
-        
+        input.style.color = 'red';
+        input.style.border = '1px solid red';
+        label.style.display = 'flex';
     }
 
-    request()
+    validator() 
     {
+        let errorStatus = true;
         this.submit.addEventListener('click', () => 
         {
-            // this.displayError();
-            this.validator();
-        })
+            //fname
+            this.firstNameInput.value === '' ? this.displayError(this.firstNameInput, this.firstNameLabel) : console.error();
+            //lname
+            this.lastNameInput.value === '' ? this.displayError(this.lastNameInput, this.lastNameLabel) : console.error();
+            //email
+            this.emailInput.value === '' ? this.displayError(this.emailInput, this.emailLabel) : console.error();
+            //query
+            // this.queryInput.value === '' ? this.displayError(this.queryInput, this.queryLabel) : console.error();
+            //message
+            this.messageInput.value === '' ? this.displayError(this.messageInput, this.messageLabel) : console.error();
+            //consent
+            this.consentInput.checked === false ? this.displayError(this.consentInput, this.consentLabel) : console.error();
+            
+        });
     }
-
 }
 
-const page = new Page();
-
-page.request();
+const user = new User();
+user.validator();
